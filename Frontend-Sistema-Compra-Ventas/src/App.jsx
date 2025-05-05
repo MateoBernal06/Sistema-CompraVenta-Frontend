eee5f3f532d65aaa784d4d6c6952090a0af07d33
JoseBernal06 <133398097+MateoBernal06@users.noreply.github.com>
2025-05-05 09:53:47 -0500

    .

--- Frontend-Sistema-Compra-Ventas/src/App.jsx
+++ Frontend-Sistema-Compra-Ventas/src/App.jsx
@@ -6,12 +6,12 @@ import 'rsuite/dist/rsuite.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // Usuario
-import { LandingPage } from './pages/mainPages/LandingPage.jsx';
-import Register from './pages/mainPages/Register.jsx';
-import Login from './pages/mainPages/Login.jsx'
-import Forgot from './pages/mainPages/Forgot.jsx';
-import RestorePassword from './pages/mainPages/RestorePassword.jsx';
-import NotFound from './pages/mainPages/NotFound.jsx';
+import { LandingPage } from './pages/MainPages/LandingPage.jsx';
+import Register from './pages/MainPages/Register.jsx';
+import Login from './pages/MainPages/Login.jsx'
+import Forgot from './pages/MainPages/Forgot.jsx';
+import RestorePassword from './pages/MainPages/RestorePassword.jsx';
+import NotFound from './pages/MainPages/NotFound.jsx';
 import DashboardAdmin from './layouts/dashboards/DashboardAdmin.jsx';
 import Auth from './layouts/Auth.jsx';
 
